b0a86b71658b99a354193d0d0159a85b7a791f51
Mekial <mekialkhan1707@gmail.com>
2026-06-08 17:16:05 +0100

    add math code

--- Maths.py
+++ Maths.py
@@ -2,3 +2,9 @@ def say_hello(name):
     print(f'Hello {name}')
 
 say_hello('kabz')
+
+def sum_two_numbers(number_1, number_2):
+    return number_1 + number_2
+
+result = sum_two_numbers(7, 8)
+print(result)
